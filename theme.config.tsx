768c8419db22dfec3c64269beaf0ecfe9549ca32
Jonathan Ou <jonathanou@Jonathans-MacBook-Pro.local>
2024-11-06 17:44:51 -0800

    q

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,17 +2,21 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <strong>Resonance</strong>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/2xOu7/resonance-nextra-docs',
   },
   chat: {
+
     link: 'https://discord.com',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/2xOu7/resonance-nextra-docs',
   footer: {
     text: 'Nextra Docs Template',
   },
+  feedback: {
+    content: null
+  }
 }
 
 export default config
